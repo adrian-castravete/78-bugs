9c88b8944b862818c89b09fb0823a3a905528d1b
Adrian Castravete <adrian.castravete@gmail.com>
2021-04-18 01:55:02 +0300

    Add gamepad/touch test

--- main.lua
+++ main.lua
@@ -14,13 +14,90 @@ fkge.c('2d', {
 	h = 8,
 })
 
-fkge.c('input', {})
+fkge.c('hitbox', '2d', {
+  c = {0, 0, 0},
+})
 
-fkge.s('input', function (e, evt)
-	if evt.keyreleased then
-		if evt.keyreleased[1] == 'escape' then
+fkge.c('input', {
+	mousePressed = 0,
+	gx = 120,
+	gy = 80,
+	gpx = 0,
+	gpy = 0,
+})
+
+fkge.s('hitbox', function (e)
+	lg.push()
+	lg.translate(e.x, e.y)
+	lg.setColor(e.c)
+	lg.rectangle('line', -e.w/2+0.5, -e.h/2+0.5, e.w, e.h)
+	lg.pop()
+end)
+
+fkge.c('spider', 'hitbox', {
+	life = 1,
+})
+
+local function spawnHitBox(x, y, b)
+	local hb = fkge.e('spider').attr {
+		x = math.floor(x),
+		y = math.floor(y),
+	}
+	if b == 1 then
+		hb.c = {1, 0, 0}
+	elseif b == 2 then
+		hb.c = {0, 1, 0}
+	elseif b == 3 then
+		hb.c = {0, 0, 1}
+	elseif b == 4 then
+		hb.c = {1, 1, 1}
+	end
+	fkge.anim(hb, 'life', 0, 3, function(v)
+		hb.c[4] = 1 - v
+		if v == 1 then
+			hb.destroy = true
+		end
+		return v
+	end)
+end
+
+fkge.s('input', function (e, evt, dt)
+	for _, kr in ipairs(evt.keyreleased or {}) do
+		if kr == 'escape' then
 			love.event.quit()
 		end
+	end
+	if evt.mousepressed then
+		e.mousePressed = evt.mousepressed[1][3]
+	end
+	if evt.mousereleased then
+		e.mousePressed = 0
+	end
+	if e.mousePressed > 0 then
+		for _, mr in ipairs(evt.mousemoved or {}) do
+			local b = e.mousePressed
+			local x, y, dx, dy = unpack(mr)
+			spawnHitBox(x, y, b)
+		end
+	end
+	for _, g in ipairs(evt.joystickaxis or {}) do
+		local gid, a, v = unpack(g)
+		if a == 1 then
+			e.gpx = v
+		end
+		if a == 2 then
+			e.gpy = v
+		end
+	end
+
+	if e.gpx ~= 0 or e.gpy ~= 0 then
+		if e.gpx ~= 0 then
+			e.gx = (e.gx + e.gpx * dt * 32) % 240
+		end
+		if e.gpy ~= 0 then
+			e.gy = (e.gy + e.gpy * dt * 32) % 160
+		end
+		spawnHitBox(e.gx, e.gy, 4)
 	end
 end)
 
@@ -29,4 +106,3 @@ fkge.scene('game', function ()
 end)
 
 fkge.scene'game'
-
