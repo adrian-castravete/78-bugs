4a224b05b17ca1827b58b97ce7fc8c1560e2cd10
Adrian Castravete <adrian.castravete@gmail.com>
2021-04-29 00:55:42 +0300

    Add gamepad cursor

--- main.lua
+++ main.lua
@@ -51,18 +51,54 @@ fkge.c('bug', 'draw', {
 	sprite = bugSprite(),
 })
 
-fkge.c('input', {
-	mousePressed = 0,
-	gx = 120,
-	gy = 80,
+fkge.c('player', 'input, draw', {
+	x = 120,
+	y = 80,
+	w = 16,
+	h = 16,
 	gpx = 0,
 	gpy = 0,
+	deploy = false,
+	gid = nil,
+	gbtn = nil,
+	sprite = fkge.spr {
+		fileName = "assets/player.png",
+		quadGen = {
+			off = {
+				x = 0,
+				y = 0,
+				w = 16,
+				h = 16,
+				c = 2,
+				n = 2,
+			},
+			on = {
+				x = 0,
+				y = 16,
+				w = 16,
+				h = 16,
+				c = 2,
+				n = 2,
+			},
+		}
+	},
+	quadName = 'off',
+	quadSpeed = 4,
 })
 
 fkge.s('draw', function (e)
 	if not e.sprite then return end
 
 	e.tick = e.tick + 1
+	if not e.quadIndex then
+		e.quadIndex = 0
+	end
+	if e.quadSpeed then
+		if e.tick >= e.quadSpeed then
+			e.quadIndex = e.quadIndex + 1
+			e.tick = 0
+		end
+	end
 
 	local w, h = e.w / 2, e.h / 2
 
@@ -77,7 +113,7 @@ fkge.s('draw', function (e)
 	end
 	local quad = nil
 	if quads then
-		quad = quads[(math.floor(e.tick / 10) % #quads) + 1]
+		quad = quads[(e.quadIndex % #quads) + 1]
 	end
 	lg.setColor(1, 1, 1, e.alpha)
 	if quad then
@@ -96,12 +132,10 @@ fkge.s('hitbox', function (e)
 	lg.pop()
 end)
 
-local ox, oy = 120, 80
-local qs = {'nw', 'n', 'ne', 'e', 'se', 's', 'sw', 'w'}
+local qs = {'se', 's', 'sw', 'w', 'nw', 'n', 'ne', 'e'}
 local function spawnBug(x, y)
-	if math.random() > 0.1 then return end
-	local a = math.atan2(y - oy, x - ox)
-	local q = math.floor(4 * a / math.pi + 4.5)
+	local a = math.random() * math.pi * 2
+	local q = math.floor(4 * a / math.pi + 0.5)
 	if q < 1 then
 		q = 8
 	end
@@ -110,7 +144,6 @@ local function spawnBug(x, y)
 		y = y,
 		quadName = qs[q],
 	}
-	ox, oy = x, y
 	fkge.anim(bug, 'life', 0, 5 * math.random(), function (ov)
 		local v = (1 - ov)
 		bug.x = bug.x + math.cos(a) * v * 0.5
@@ -129,19 +162,29 @@ fkge.s('input', function (e, evt, dt)
 			love.event.quit()
 		end
 	end
-	if evt.mousepressed then
-		e.mousePressed = evt.mousepressed[1][3]
-	end
-	if evt.mousereleased then
-		e.mousePressed = 0
-	end
-	if e.mousePressed > 0 then
-		for _, mr in ipairs(evt.mousemoved or {}) do
-			local b = e.mousePressed
-			local x, y, dx, dy = unpack(mr)
-			spawnBug(x, y, b)
+
+	for _, g in ipairs(evt.joystickpressed or {}) do
+		local gid, b = unpack(g)
+		if not e.gid then
+			e.gid = gid
+		end
+		if gid == e.gid then
+			if not e.gbtn then
+				e.gbtn = b
+			end
+			if e.gbtn == b then
+				e.deploy = true
+			end
 		end
 	end
+
+	for _, g in ipairs(evt.joystickreleased or {}) do
+		local gid, b = unpack(g)
+		if e.gid == gid and e.gbtn == b then
+			e.deploy = false
+		end
+	end
+
 	for _, g in ipairs(evt.joystickaxis or {}) do
 		local gid, a, v = unpack(g)
 		if a == 1 then
@@ -154,17 +197,22 @@ fkge.s('input', function (e, evt, dt)
 
 	if e.gpx ~= 0 or e.gpy ~= 0 then
 		if e.gpx ~= 0 then
-			e.gx = (e.gx + e.gpx * dt * 32) % 240
+			e.x = (e.x + e.gpx * dt * 32) % 240
 		end
 		if e.gpy ~= 0 then
-			e.gy = (e.gy + e.gpy * dt * 32) % 160
+			e.y = (e.y + e.gpy * dt * 32) % 160
 		end
-		spawnBug(e.gx, e.gy, 4)
+	end
+end)
+
+fkge.s('player', function (e)
+	if e.deploy then
+		spawnBug(e.x, e.y, 4)
 	end
 end)
 
 fkge.scene('game', function ()
-	fkge.e('input')
+	fkge.e('player')
 end)
 
 fkge.scene'game'
